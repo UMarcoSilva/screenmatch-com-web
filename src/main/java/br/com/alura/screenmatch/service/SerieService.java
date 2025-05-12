160f24d0a9e9b5607a9ce7d1c71a090726c0bc9d
Seu Nome <marcoadias.silva@gmail.com>
2025-05-11 23:37:05 -0300

    feat: Controller, Live reload, DTO, CorsConfiguration

---
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -0,0 +1,38 @@
+package br.com.alura.screenmatch.service;
+
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository serieRepository;
+
+    @GetMapping
+    public List<SerieDTO> obterTodasAsSeries() {
+        return serieRepository.findAll().stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SerieDTO> obterTop5series() {
+        return serieRepository.findTop5ByOrderByAvaliacaoDesc()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series) {
+        
+    }
+}
